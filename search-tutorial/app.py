d6742013f7124ca2833b3edd32af4a6c4d95631f
Jaap Brasser <noreply@jaapbrasser.com>
2026-01-10 11:23:15 +0300

    Completed k-Nearest Neighbor Search

--- search-tutorial/app.py
+++ search-tutorial/app.py
@@ -15,28 +15,13 @@ def handle_search():
     filters, parsed_query = extract_filters(query)
     from_ = request.form.get('from_', type=int, default=0)
 
-    if parsed_query:
-        search_query = {
-            'must': {
-                'multi_match': {
-                    'query': parsed_query,
-                    'fields': ['name', 'summary', 'content'],
-                }
-            }
-        }
-    else:
-        search_query = {
-            'must': {
-                'match_all': {}
-            }
-        }
-
     results = es.search(
-        query={
-            'bool': {
-                **search_query,
-                **filters
-            }
+        knn={
+            'field': 'embedding',
+            'query_vector': es.get_embedding(parsed_query),
+            'k': 10,
+            'num_candidates': 50,
+            **filters,
         },
         aggs={
             'category-agg': {
