91bbd4ed50d1e63c5fe8f2baac9597d3ed4fad0f
Jaap Brasser <noreply@jaapbrasser.com>
2026-01-10 10:00:48 +0300

    Completed Filters

--- search-tutorial/app.py
+++ search-tutorial/app.py
@@ -12,14 +12,34 @@ def index():
 @app.post('/')
 def handle_search():
     query = request.form.get('query', '')
+    filters, parsed_query = extract_filters(query)
     from_ = request.form.get('from_', type=int, default=0)
+
+    if parsed_query:
+        search_query = {
+            'must': {
+                'multi_match': {
+                    'query': parsed_query,
+                    'fields': ['name', 'summary', 'content'],
+                }
+            }
+        }
+    else:
+        search_query = {
+            'must': {
+                'match_all': {}
+            }
+        }
+
     results = es.search(
         query={
-            'multi_match': {
-                'query': query,
-                'fields': ['name', 'summary', 'content'],
+            'bool': {
+                **search_query,
+                **filters
             }
-        }, size=5, from_=from_
+        },
+        size=5,
+        from_=from_
     )
     return render_template('index.html', results=results['hits']['hits'],
                            query=query, from_=from_,
@@ -38,3 +58,33 @@ def reindex():
     response = es.reindex()
     print(f'Index with {len(response["items"])} documents created '
           f'in {response["took"]} milliseconds.')
+
+def extract_filters(query):
+    filters = []
+
+    filter_regex = r'category:([^\s]+)\s*'
+    m = re.search(filter_regex, query)
+    if m:
+        filters.append({
+            'term': {
+                'category.keyword': {
+                    'value': m.group(1)
+                }
+            },
+        })
+        query = re.sub(filter_regex, '', query).strip()
+
+    filter_regex = r'year:([^\s]+)\s*'
+    m = re.search(filter_regex, query)
+    if m:
+        filters.append({
+            'range': {
+                'updated_at': {
+                    'gte': f'{m.group(1)}||/y',
+                    'lte': f'{m.group(1)}||/y',
+                }
+            },
+        })
+        query = re.sub(filter_regex, '', query).strip()
+
+    return {'filter': filters}, query
